25078786314e1f9476c215f42bbb59316b69c014
Jonathan Phipps <jonathanmp25@gmail.com>
2018-05-25 09:25:12 -0500

    Set up redux and change file structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
-import FormContainer from "./js/components/container/FormContainer";
+import FormContainer from "./react/components/container/FormContainer";
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
+import reducer from './redux/reducer';
+
+const loggerMiddleware = createLogger();
+
+const store = createStore(
+  reducer,
+  applyMiddleware(
+    thunkMiddleware,
+    loggerMiddleware
+  )
+);
 
 ReactDOM.render(
-  <FormContainer />,
+  <Provider store={store}>
+    <FormContainer />
+  </Provider>,
   document.getElementById("create-article-form")
 );
---
+++ src/redux/reducer.js
@@ -0,0 +1,13 @@
+import {
+  ACTION
+} from './actions';
+
+const initialState = {
+
+};
+
+const reducer = (state = initialState, action) => {
+  return state;
+}
+
+export default reducer;
